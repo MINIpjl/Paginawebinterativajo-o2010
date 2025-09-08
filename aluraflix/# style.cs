e4c9b3a26b12b54a144979313239f474e883cbfe
MINIpjl <joao.ramos.santos01@escola.pr.gov.br>
2025-09-08 14:39:02 -0300

    adicao de codigo

--- aluraflix/# style.cs
+++ aluraflix/# style.cs
@@ -1,3 +1,44 @@
 body {
-    color:rgb(0, 11, 75)
+    color:rgb(0, 11, 75);
+    background:rgb(0, 255, 149);
+    margin: 0px;
+    font-family:Georgia, 'Times New Roman' , Times, serif;
+}
+hearder {
+    border-bottom: solid 2xp rgb(42, 122, 228);
+    padding: 20px;
+    font-size: 32px;
+    color: rgb(9, 0, 11);
+}
+.chamada   {
+    background: rgba(246, 1, 9, 1)
+    padding-bottom: 80px;
+    padding-top: 80px;
+    display: flex;
+    justify-content: center;
+}
+
+.chamada-texto {
+    margin:5%;
+}
+
+h1{font-size:40px;
+}
+p{font-size:20px;
+
+}
+img {
+    height:200px;
+
+
+}
+.categoria-videos {
+    display: flex;
+    overflow:auto;
+    gap:10px;
+
+}
+.categoria {
+    padding-left: 20px;
+    padding-right:20px;
 }
